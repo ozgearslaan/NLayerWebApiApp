5392931b81c73e2e9dcba4ba0e035d1f058abd40
ozgearslaan <34003286+ozgearslaan@users.noreply.github.com>
2023-04-12 16:47:52 +0300

    Update CategorySeed.cs

--- NLayer.Repository/Seeds/CategorySeed.cs
+++ NLayer.Repository/Seeds/CategorySeed.cs
@@ -11,15 +11,8 @@ namespace NLayer.Repository.Seeds
 {
     public class CategorySeed : IEntityTypeConfiguration<Category>
     {
-        //BURADAKİ İŞLEMİ APPDBCONTEXTİN İÇİNDE DE YAPABİLİRİZ
-        //ama amaç dbcontexti kirletmemek
         public void Configure(EntityTypeBuilder<Category> builder)
         {
-            //seed yaparken
-            //migration esnasında default kayıtlarımızın oluşmasını istiyorsak
-            //id'leri kendimizin vermesi lazım, normalde entity bi alan new anahtar sözcüğü oluşturarak
-            //dbye kaydettiğimizde sql server tarafında birer birer artan bir değerle
-            //id alanı belirlenecek ancak seed data esnasında id yi kendimiz vermeliyiz
             builder.HasData(
                 new Category { Id = 1, Name = "Kalemler" }, 
                 new Category { Id = 2, Name = "Kitaplar" }, 
