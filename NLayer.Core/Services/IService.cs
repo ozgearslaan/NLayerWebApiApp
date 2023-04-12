d41556a46b6ad8e68e895dfbd1598b68addc8368
ozgearslaan <34003286+ozgearslaan@users.noreply.github.com>
2023-04-12 16:45:07 +0300

    Update IService.cs

--- NLayer.Core/Services/IService.cs
+++ NLayer.Core/Services/IService.cs
@@ -9,22 +9,15 @@ namespace NLayer.Core.Services
 {
     public interface IService<T> where T : class
     {
-        //IGenericRepository ile aynı methodlar ancak farklı işlenebilir
         Task<T> GetByIdAsync(int id);
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
 
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> AddAsync(T entity);
-        //repositoryde void döndük çünkü dbcontext efcoreun async yoktu
-        //ama veritabanına bu değişiklikleri serviste yansıtacağımız için
-        //async a dönüştürdük
         Task UpdateAsync(T entity);
         Task RemoveAsync(T entity);
         Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
         Task RemoveRangeAsync(IEnumerable<T> entities);
-        //iservice'te veritabanına bu değişiklikleri yansıtacağımız için
-        ////savechangeasync methodunu kullanacağımız için
-        //bunları Task'e yani asenkrona dönüştürüyoruz
     }
 }
