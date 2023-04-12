d1336fc48cb7a24c652584d42285652bddbf2673
ozgearslaan <34003286+ozgearslaan@users.noreply.github.com>
2023-04-12 16:41:32 +0300

    Update BaseEntity.cs

--- NLayer.Core/Models/BaseEntity.cs
+++ NLayer.Core/Models/BaseEntity.cs
@@ -8,10 +8,8 @@ namespace NLayer.Core.Models
 {
     public abstract class BaseEntity
     {
-        //bir nesne örneği alınmasını istemediğim için abstract dedim
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
-        //ilk eklenen kayıtta bu veri olmayacak
     }
 }
