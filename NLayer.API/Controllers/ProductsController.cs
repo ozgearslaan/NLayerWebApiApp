f719aa6de188207b995114ce22da6d47766688a5
ozgearslaan <34003286+ozgearslaan@users.noreply.github.com>
2023-04-12 16:38:08 +0300

    Update ProductsController.cs

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -15,8 +15,6 @@ namespace NLayer.API.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IProductService _service;
-        //controllerlar sadece Service katmanını bilir
-
         
         public ProductsController(IMapper mapper, IProductService productService)
         {
@@ -26,15 +24,11 @@ namespace NLayer.API.Controllers
         
         //GET api/products/GetProductsWithCategory
         [HttpGet("[action]")]
-        //isim belirtmemize gerek yok [HttpGet("[action]")]
         public async Task<IActionResult> GetProductsWithCategory()
         {
             return CreateActionResult(await _service.GetProductWithCategory());
-            //aşağıdaki methodlardaki 2 satırı gerçekten olması gereken yerde servis katmanında yaptık
         }
-        //genericler dtoya dönüştürmediği için içinde dtoya dönüştürüyoruz ancak yukarıda 
-        //custom olduğu için özelleştirildiği için direkt api in istediği datayı tek satırda
-        //dönebiliriz
+       
 
 
         //GET api/products
@@ -42,13 +36,9 @@ namespace NLayer.API.Controllers
         public async Task<IActionResult> All()
         {
             var products = await _service.GetAllAsync();
-            //bu bir entitydir geriye dto dönmek lazım
             var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
-            //ienumerable dönüyor liste dönüştürmek için tolist
-            //return Ok(CustomResponseDto<List<ProductDto>>.Success(200, productDtos));
             return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
-        }//geriye tek bir dto dönücez o da customresponsedto dönecek
-         //dönme işlemini custombasecontrollerdan alıyor CreateActionResult
+        }
 
 
         //GET /api/products/5
@@ -57,18 +47,7 @@ namespace NLayer.API.Controllers
         public async Task<IActionResult> GetById(int id)
         {
             var product = await _service.GetByIdAsync(id);
-            //bu bir entitydir geriye dto dönmek lazım
-            
-           // if(product==null)
-           // {
-           //     return CreateActionResult(CustomResponseDto<ProductDto>.Fail(400,"Bu id'ye sahip ürün bulunamadı."));
-           // }
-           //yukarıda null kontrolü yaptık ancak kötü bir yol çünkü bunu birçok entityde de ayrı ayrı yapmamız gerekebilir
-           //bunu servis katmanında çağıracağız
-            
             var productsDto = _mapper.Map<ProductDto>(product);
-            //ienumerable dönüyor liste dönüştürmek için tolist
-            //return Ok(CustomResponseDto<List<ProductDto>>.Success(200, productDtos));
             return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productsDto));
         }
 
@@ -76,10 +55,7 @@ namespace NLayer.API.Controllers
         public async Task<IActionResult> Save(ProductDto productDto)
         {
             var product = await _service.AddAsync(_mapper.Map<Product>(productDto));
-            //bu bir entitydir geriye dto dönmek lazım
             var productsDto = _mapper.Map<ProductDto>(product);
-            //ienumerable dönüyor liste dönüştürmek için tolist
-            //return Ok(CustomResponseDto<List<ProductDto>>.Success(200, productDtos));
             return CreateActionResult(CustomResponseDto<ProductDto>.Success(201, productsDto));
             //201 created
 
@@ -98,9 +74,8 @@ namespace NLayer.API.Controllers
         public async Task<IActionResult> Remove(int id)
         {
             var product = await _service.GetByIdAsync(id);
-            //bu idye sahip ürün var mı diye if leri yazmayacağız exceptionları ayrı yerden fırlatacağız
             await _service.RemoveAsync(product);
             return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
-        }//remove ve updatede geriye bir şey dönmeye gerek yok o yüzden nocontentdto döndü
+        }
     }
 }
