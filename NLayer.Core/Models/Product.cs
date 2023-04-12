ce377134cfeb085708efeea6356041b201114e6c
ozgearslaan <34003286+ozgearslaan@users.noreply.github.com>
2023-04-12 16:42:04 +0300

    Update Product.cs

--- NLayer.Core/Models/Product.cs
+++ NLayer.Core/Models/Product.cs
@@ -8,17 +8,11 @@ namespace NLayer.Core.Models
 {
     public class Product : BaseEntity
     {
-        //public Product(string name)
-        // {
-        //     Name = name ?? throw new ArgumentNullException(nameof(Name));
-        //  } //eğer null olursa exception hatası fırlat
         public string Name { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
-        //bire çok ilişki o yüzden category id gerekli
         public int CategoryId { get; set; }
         //foreign key
-        //her bir productın da bir tane categorysi vardır
         public Category Category { get; set; }
         //navigation property
         public ProductFeature ProductFeature { get; set; }
