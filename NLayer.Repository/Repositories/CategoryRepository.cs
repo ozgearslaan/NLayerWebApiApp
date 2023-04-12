85d982dd0195491da199ff1048ff303702a0f149
ozgearslaan <34003286+ozgearslaan@users.noreply.github.com>
2023-04-12 16:46:46 +0300

    Update CategoryRepository.cs

--- NLayer.Repository/Repositories/CategoryRepository.cs
+++ NLayer.Repository/Repositories/CategoryRepository.cs
@@ -18,8 +18,7 @@ namespace NLayer.Repository.Repositories
         public async Task<Category> GetSingleCategoryByIdWithProductsAsync(int categoryId)
         {
             return await _context.Categories.Include(x=> x.Products).Where(x=>x.Id == categoryId).SingleOrDefaultAsync();
-            //bu idden 4-5 tane varsa hata döner
-            //firstordefault yazarsak 4-5 tane varsa ilkini bulup döndürür
+
         }
     }
 }
