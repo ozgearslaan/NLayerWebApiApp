54edc326544972f02b85d7f3a1f5d0ebf63873a6
ozgearslaan <34003286+ozgearslaan@users.noreply.github.com>
2023-04-12 16:36:41 +0300

    Update CategoriesController.cs

--- NLayer.API/Controllers/CategoriesController.cs
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -6,9 +6,7 @@ using NLayer.Core.Services;
 
 namespace NLayer.API.Controllers
 {
-    //[ValidateFilterAttribute] ama sürekli eklemek sıkıntı
-    //o yüzden global tanımlamak lazım bir şey globalse
-    //programcsde tanımlamak lazım 
+    
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : CustomBaseController
@@ -23,7 +21,7 @@ namespace NLayer.API.Controllers
         //api/categories/GetSingleCategoryByIdWithProducts/2
         [HttpGet("[action]/{categoryId}")]
         public async Task<IActionResult> GetSingleCategoryByIdWithProducts(int categoryId)
-        {//async olduğu için taskle kapsülledik
+        {
             return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProducts(categoryId));
         }
 
