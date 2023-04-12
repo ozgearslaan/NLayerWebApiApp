5285212f83e81e265e17b2b2f97f62cca77b8844
ozgearslaan <34003286+ozgearslaan@users.noreply.github.com>
2023-04-12 16:51:05 +0300

    Update ProductDtoValidator.cs

--- NLayer.Service/Validations/ProductDtoValidator.cs
+++ NLayer.Service/Validations/ProductDtoValidator.cs
@@ -13,9 +13,7 @@ namespace NLayer.Service.Validations
         public ProductDtoValidator()
         {
             RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
-
             RuleFor(x => x.Price).InclusiveBetween(1,int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
-            //default değerleri var 0 biz notnull notempty ile kontrol edemeyiz
             RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
             RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
 
