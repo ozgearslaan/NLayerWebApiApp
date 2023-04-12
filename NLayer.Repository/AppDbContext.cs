3a0c5e03f32daacbfdcc489718a144c09ef1b0c2
ozgearslaan <34003286+ozgearslaan@users.noreply.github.com>
2023-04-12 16:48:58 +0300

    Update AppDbContext.cs

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -13,47 +13,17 @@ namespace NLayer.Repository
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        //options alıyor çünkü veritabanı yolunu startup dosyasından vereceğiz.
-        //kim için options appdbcontext için sonra base ile optionsa gönderiyoruz.
         {
 
         }
 
-        //her bir entitye karşılık bir dbset oluşturacağız
         public DbSet<Category> Categories{get;set;}
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
-        //productfeature ürünlerini product üstünden eklenebilir
-        
-        //entityler ile ilgili ayarları
-        //yapabilmrk için migration esnasında override etmemiz gereken methodumuz var
-        //kkategorideki idyi primary key olarak belirleyebiliriz 
-        //efcore'daki gibi id isimlendirmesi yapmazsak bu şekilde belirtebiliriz
-        //dbkontexte tanımlarsak cok karışık bir dosya olur o yüzden
-        //her entity için ayarları ayrı bir dosyada yapmak lazım
-        //configuration klasöründe yapıldığı gibi
-        
-        //public override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-          //  modelBuilder.Entity<Category>().HasKey(x => x.Id);
-            //base.OnModelCreating(modelBuilder);
-        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //entity ile ilgili ayarları yapmak için override etmemiz gereken bir method var
-
-
-            //.applyconfigurationfromassemnly
-            //yani diyor ki git assembly içerisindeki tüm configuration dosyalarını oku
-            //yukarıdaki interfacei implemente ettiği için tüm configuration dosyalarını bulur
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-            //çalışmış olduğu assemblyi tarama getexecuting
-
-            //tek birini uygulamak istesek
-            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
-
-
             modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
                 {
                     Id=1,
@@ -70,10 +40,6 @@ namespace NLayer.Repository
                     Width = 500,
                     ProductId = 2
                 });
-
-
-
-
             base.OnModelCreating(modelBuilder);
         }
 
