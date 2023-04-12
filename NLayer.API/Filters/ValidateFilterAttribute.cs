3b9c4b6d9bed9f54564bc1962cf03a5d376d6962
ozgearslaan <34003286+ozgearslaan@users.noreply.github.com>
2023-04-12 16:38:38 +0300

    Update ValidateFilterAttribute.cs

--- NLayer.API/Filters/ValidateFilterAttribute.cs
+++ NLayer.API/Filters/ValidateFilterAttribute.cs
@@ -7,17 +7,14 @@ namespace NLayer.API.Filters
     public class ValidateFilterAttribute:ActionFilterAttribute
     {
         public override void OnActionExecuting(ActionExecutingContext context)
-            //model çalışırken ki methodu ovveride edicez
         {
             if(!context.ModelState.IsValid)
-                //hata var mı kontrol etme
             {
                 var errors = context.ModelState.Values.SelectMany(x => x.Errors).Select(x=>x.ErrorMessage).ToList();
 
                 context.Result = new BadRequestObjectResult(CustomResponseDto<NoContentDto>.Fail(400, errors));
-                //fail döneceğim için no content
+
             }
-            //fluentvalidation modelstatela entegre
         }
     }
 }
