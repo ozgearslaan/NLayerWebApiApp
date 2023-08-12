54b8e26b2b0ddcb5eb9cbd92ec348548ee2b4c0a
ozgearslaan <34003286+ozgearslaan@users.noreply.github.com>
2023-08-12 13:53:50 +0300

    Update Program.cs

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -4,18 +4,13 @@ using Autofac.Extensions.DependencyInjection;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Extensions;
 using NLayer.API.Filters;
 using NLayer.API.Middlewares;
 using NLayer.API.Modules;
-using NLayer.Core.Repositories;
-using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
 using NLayer.Repository;
-using NLayer.Repository.Repositories;
 using NLayer.Repository.UnitOfWorks;
 using NLayer.Service.Mapping;
-using NLayer.Service.Services;
 using NLayer.Service.Validations;
 using System.Reflection;
 
@@ -29,12 +24,31 @@ builder.Services.AddControllers(options => options.Filters.Add(new ValidateFilte
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
     options.SuppressModelStateInvalidFilter = true;
-});
+});//validationın kendi döndüğü filtreyi kendi filtremizle baskıladık
+//mvc tarafında filterı baskılamamıza gerek yok 
+//apida default olarak açık çünkü
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();//cachele haberdar ettik
+
+//notfoundfilterı program cs e de ekledik
+
+//ayrı repositoryler kullanıyoruz ve ayrı ayrı interfaceler var onları da eklemek lazım ki migration yapabilelim
+
 builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+//ıunitofwork ile karşılaşırsan unitofworkten nesne alacaksın
+//genericleri typeofla ekleyeceğiz, birden fazla generic alsaydı <,> veya <,,> koyacaktık
+//builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+//T aldığı için <>
+//mapping
 builder.Services.AddAutoMapper(typeof(MapProfile));
 
+//builder.Services.AddScoped<IProductService,ProductService>();
+//builder.Services.AddScoped<IProductRepository,ProductRepository>();
+//productwithcategory için ekledik
+//builder.Services.AddScoped<ICategoryService, CategoryService>();
+//.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
@@ -43,10 +57,16 @@ builder.Services.AddDbContext<AppDbContext>(x =>
         option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
     
     });
+    //migration dosyaları repository oluşacak ve appdbcontextte repository katmanında
+    //o yüzden appdbcontextin bulunmuş olduğu assemblyi api tarafında uygulamaya haber vermemiz lazım
+    //optionla içine girilir
 });
 
 builder.Host.UseServiceProviderFactory
     (new AutofacServiceProviderFactory());
+//autofac kütüphanesi
+//modül ekleyeceğiz ve dinamik olarak ekleme yapabileceğiz.
+
 builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule()));
 
 
